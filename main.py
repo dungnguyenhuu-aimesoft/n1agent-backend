cebdb100f3e87ccac6c6ddaa2b4e5808c4527a0c
dungnguyenhuu-aime <dungnguyenhuu@aimesoft.com>
2025-12-31 11:28:13 +0700

    Make server port configurable via CLI args

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import argparse
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, default=8000)
+    args = parser.parse_args()
+
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=args.port,
+        reload=True
+    )
+
